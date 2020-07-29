b704a959f25e746536ecc7aea7ddc31c2718278e
luojunwei <34501477+luojunwei@users.noreply.github.com>
2020-07-29 09:48:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 
 
 int main(int argc,char** argv) {
-	int maxSize = 2000;
+	int maxSize = 50000;
 	char * line = (char *)malloc(sizeof(char)*maxSize);
 	
 	char * resultOutPutDirectory = (char *)malloc(sizeof(char)*50);
